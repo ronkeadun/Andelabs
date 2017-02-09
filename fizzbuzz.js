ccbb036f6d304cd81ddb3ed8118eaa9bde741d5b
ronkeadun <ronkeadun1@yahoo.com>
2017-02-09 08:38:47 +0100

    third commit

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,10 +1,11 @@
 //Simple FizzBuzz program with numbers 1 to 20
-
-for (var i = 1; i <= 20; i++){
-	if ((i % 3 === 0) && (i % 5 === 0))
-		console.log("FizzBuzz");
-	else if (i % 3 === 0)
-		console.log ("Fizz");
-	else if (i % 5 === 0)
-		console.log("Buzz");
+function fizzBuzz(){
+	for (var i = 1; i <= 20; i++){
+		if ((i % 3 === 0) && (i % 5 === 0))
+			return ("FizzBuzz");
+		else if (i % 3 === 0)
+			return ("Fizz");
+		else if (i % 5 === 0)
+			return ("Buzz");
+	}
 }
