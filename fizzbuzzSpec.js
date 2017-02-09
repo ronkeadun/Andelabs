73e0ead13c37739999a4d52d8eb245da7e6b3bec
ronkeadun <ronkeadun1@yahoo.com>
2017-02-09 09:05:41 +0100

    fifth commit

--- fizzbuzzSpec.js
+++ fizzbuzzSpec.js
@@ -4,8 +4,16 @@ describe("Fizz Buzz tests ", function() {
     expect(fizzBuzz(3)).toBe('Fizz');
   });
 
+  it("should return 'Fizz' for number divisible by 3", function() {
+    expect(fizzBuzz(21)).toBe('Fizz');
+  });
+
   it("should return 'Buzz' for number divisible by 5", function() {
     expect(fizzBuzz(5)).toBe('Buzz');
+  });
+
+  it("should return 'Buzz' for number divisible by 5", function() {
+    expect(fizzBuzz(25)).toBe('Buzz');
   });
 
   it("should return 'FizzBuzz' for 15", function() {
